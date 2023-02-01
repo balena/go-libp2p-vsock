5ab34ffdbfcb77083a80b95b8cec4456ac2c92a1
Guilherme Balena Versiani <guibv@mailbox.org>
2023-02-01 19:39:32 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+# go-libp2p-vsock
+
+> go-libp2p's VSOCK transport
+
+Package `go-libp2p-vsock` is a libp2p [transport](https://pkg.go.dev/github.com/libp2p/go-libp2p/core/transport). It uses `virtio-vsock` enable communication channel to relays at Nitro Enclaves.
+
+## Install
+
+```sh
+go get github.com/balena/go-libp2p-vsock
+```
+
+## Contribute
+
+Feel free to join in. All welcome. Open an [issue](https://github.com/balena/go-libp2p-vsock/issues)!
+
+This repository falls under the libp2p [Code of Conduct](https://github.com/libp2p/community/blob/master/code-of-conduct.md).
+
+### Want to hack on libp2p?
+
+[![](https://cdn.rawgit.com/libp2p/community/master/img/contribute.gif)](https://github.com/libp2p/community/blob/master/CONTRIBUTE.md)
+
+## License
+
+[MIT](LICENSE) © 2023 Guilherme Versiani
---
+++ vsock.go
@@ -0,0 +1,142 @@
+package libp2pvsock
+
+import (
+	"context"
+
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/transport"
+	"github.com/libp2p/go-libp2p/p2p/net/reuseport"
+
+	mavs "github.com/balena/go-multiaddr-vsock"
+	mavsnet "github.com/balena/go-multiaddr-vsock/net"
+	logging "github.com/ipfs/go-log/v2"
+	ma "github.com/multiformats/go-multiaddr"
+	mafmt "github.com/multiformats/go-multiaddr-fmt"
+	manet "github.com/multiformats/go-multiaddr/net"
+)
+
+var log = logging.Logger("vsock-tpt")
+
+type Option func(*VsockTransport) error
+
+func DisableReuseport() Option {
+	return func(tr *VsockTransport) error {
+		tr.disableReuseport = true
+		return nil
+	}
+}
+
+// VsockTransport is the VSOCK transport.
+type VsockTransport struct {
+	// Connection upgrader for upgrading insecure stream connections to
+	// secure multiplex connections.
+	upgrader transport.Upgrader
+
+	disableReuseport bool // Explicitly disable reuseport.
+
+	rcmgr network.ResourceManager
+
+	reuse reuseport.Transport
+}
+
+var _ transport.Transport = &VsockTransport{}
+
+// New creates a VSOCK transport object that tracks dialers and listeners
+// created.
+func New(upgrader transport.Upgrader, rcmgr network.ResourceManager, opts ...Option) (*VsockTransport, error) {
+	if rcmgr == nil {
+		rcmgr = &network.NullResourceManager{}
+	}
+	tr := &VsockTransport{
+		upgrader: upgrader,
+		rcmgr:    rcmgr,
+	}
+	for _, o := range opts {
+		if err := o(tr); err != nil {
+			return nil, err
+		}
+	}
+	return tr, nil
+}
+
+var dialMatcher = mafmt.And(mafmt.Base(mavs.P_VSOCK), mafmt.Base(ma.P_TCP))
+
+// CanDial returns true if this transport believes it can dial the given
+// multiaddr.
+func (t *VsockTransport) CanDial(addr ma.Multiaddr) bool {
+	return dialMatcher.Matches(addr)
+}
+
+func (t *VsockTransport) maDial(ctx context.Context, raddr ma.Multiaddr) (manet.Conn, error) {
+	// Context is discarded for now in the hope VSOCK connections
+	// complete as fast as possible.
+	return mavsnet.Dial(raddr)
+}
+
+// Dial dials the peer at the remote address.
+func (t *VsockTransport) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.ID) (transport.CapableConn, error) {
+	connScope, err := t.rcmgr.OpenConnection(network.DirOutbound, true, raddr)
+	if err != nil {
+		log.Debugw("resource manager blocked outgoing connection", "peer", p, "addr", raddr, "error", err)
+		return nil, err
+	}
+
+	c, err := t.dialWithScope(ctx, raddr, p, connScope)
+	if err != nil {
+		connScope.Done()
+		return nil, err
+	}
+	return c, nil
+}
+
+func (t *VsockTransport) dialWithScope(ctx context.Context, raddr ma.Multiaddr, p peer.ID, connScope network.ConnManagementScope) (transport.CapableConn, error) {
+	if err := connScope.SetPeer(p); err != nil {
+		log.Debugw("resource manager blocked outgoing connection for peer", "peer", p, "addr", raddr, "error", err)
+		return nil, err
+	}
+	conn, err := t.maDial(ctx, raddr)
+	if err != nil {
+		return nil, err
+	}
+	direction := network.DirOutbound
+	if ok, isClient, _ := network.GetSimultaneousConnect(ctx); ok && !isClient {
+		direction = network.DirInbound
+	}
+	return t.upgrader.Upgrade(ctx, t, conn, direction, p, connScope)
+}
+
+// UseReuseport returns true if reuseport is enabled and available.
+func (t *VsockTransport) UseReuseport() bool {
+	return !t.disableReuseport && ReuseportIsAvailable()
+}
+
+func (t *VsockTransport) maListen(laddr ma.Multiaddr) (manet.Listener, error) {
+	if t.UseReuseport() {
+		return t.reuse.Listen(laddr)
+	}
+	return manet.Listen(laddr)
+}
+
+// Listen listens on the given multiaddr.
+func (t *VsockTransport) Listen(laddr ma.Multiaddr) (transport.Listener, error) {
+	list, err := t.maListen(laddr)
+	if err != nil {
+		return nil, err
+	}
+	return t.upgrader.UpgradeListener(t, list), nil
+}
+
+// Protocols returns the list of terminal protocols this transport can dial.
+func (t *VsockTransport) Protocols() []int {
+	return []int{mavs.P_VSOCK}
+}
+
+// Proxy always returns false for the TCP transport.
+func (t *VsockTransport) Proxy() bool {
+	return false
+}
+
+func (t *VsockTransport) String() string {
+	return "VSOCK"
+}
